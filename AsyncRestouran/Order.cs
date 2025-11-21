e1836578fe8d1f671bf73c5af23389d2e1ee4840
Andrey Prokudin <107585116+AndreyProkydin@users.noreply.github.com>
2025-11-21 21:14:28 +0200

    Laboratorna 2

---
+++ AsyncRestouran/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncRestouran
+{
+    public class Order
+    {
+        private static readonly Random _random = new Random();
+        public Guid Id { get; set; }
+        public string TypeOrder {  get; set; }
+        public DateTime DateOrder { get; set; }
+        public float OrderPrice { get; set; }
+        public List<String> OrderPosition { get; set; }
+
+
+        public static Order CreateNew()
+        {
+            var orderTypes = new[] { "Dine-in", "Takeaway", "Delivery" };
+            var menuItems = new[] { "Pizza", "Salad", "Steak", "Burger", "Pasta", "Soup" };
+
+            var positions = new List<string>();
+            int itemsCount = _random.Next(2, 4);
+            for (int i = 0; i < itemsCount; i++)
+            {
+                positions.Add(menuItems[_random.Next(menuItems.Length)]);
+            }
+
+            return new Order
+            {
+                TypeOrder = orderTypes[_random.Next(orderTypes.Length)],
+                DateOrder = DateTime.Now.AddDays(-_random.Next(0, 30)), 
+                OrderPrice = (float)Math.Round(_random.Next(150, 2000) * _random.NextDouble(), 2), 
+                OrderPosition = positions
+            };
+        }
+    }
+}
---
+++ UnitTest/Test1.cs
@@ -0,0 +1,42 @@
+﻿using AsyncRestouran;
+
+namespace UnitTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private ICrudServiceAsync<Order> _service;
+        private readonly string _testFilePath = "orders_test.json";
+
+        [TestInitialize] 
+        public void Setup()
+        {
+          
+            _service = new OrderCrud(_testFilePath); 
+
+            if (File.Exists(_testFilePath))
+            {
+                File.Delete(_testFilePath);
+            }
+        }
+
+        [TestMethod]
+        public async Task CreateAsync_Should_AddItem_And_IncreaseCount()
+        {
+            var order = Order.CreateNew();
+            Assert.AreEqual(0, _service.Count()); 
+            await _service.CreateAsync(order);
+   
+            Assert.AreEqual(1, _service.Count()); 
+        }
+        [TestCleanup] 
+        public void Cleanup()
+        {
+            
+            if (File.Exists(_testFilePath))
+            {
+                File.Delete(_testFilePath);
+            }
+        }
+    }
+}
